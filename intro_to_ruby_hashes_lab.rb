4bf888670e1ddb42c37e8c6e7ac756d8539819d3
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-08-30 00:32:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   pioneer = Hash.new
-  pioneer[:name] = 'Grace Hopper'
+ # pioneer[:name] = 'Grace Hopper'
 end
 
 def id_generator
